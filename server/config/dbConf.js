8394cbc6cd374addbf869882f24146260e7bd4ad
Vishnevskyi <76432370+Vishnevskyi@users.noreply.github.com>
2022-08-30 19:23:53 +0300

    Update dbConf.js

--- server/config/dbConf.js
+++ server/config/dbConf.js
@@ -1,6 +1,6 @@
 module.exports = {
-    host: "remotemysql.com",
-    user: "H1UNEs3Sez",
-    database: "H1UNEs3Sez",
-    password: "MQfDd4571q"
+    host: "",
+    user: "",
+    database: "",
+    password: ""
 }
